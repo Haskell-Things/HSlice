4f285aa2126f3f525f0e8d2572409ee298f6426f
Noah Halford <noah.halford@gmail.com>
2016-04-22 21:13:26 -0500

    Basic functionality of intersections working

--- slicer.hs
+++ slicer.hs
@@ -1,13 +1,15 @@
 module Main where
 
 import Data.Char (toUpper, toLower, isSpace)
+import Data.List (nub)
+import Data.Maybe (fromJust)
 
 ----------------------------------------------------------
 ------------ Overhead (data structures, etc.) ------------
 ----------------------------------------------------------
 
 -- A Point data structure
-data Point a = Point { x :: a, y :: a, z :: a }
+data Point a = Point { x :: a, y :: a, z :: a } deriving Eq
 
 instance Functor Point where
     fmap f (Point x y z) = Point (f x) (f y) (f z)
@@ -22,15 +24,19 @@ instance (Show a) => Show (Point a) where
 
 -- Data structure for a line segment in the form (x,y,z) = (x0,y0,z0) + t(mx,my,mz)
 -- t should run from 0 to 1, so the endpoints are (x0,y0,z0) and (x0 + mx, y0 + my, z0 + mz)
--- TODO (1): Does it make sense to have separate types for lines and line segments? Will we
--- ever need an infinite line?
--- TODO (2): Is this the best representation, or does it make sense to just have a line
+-- TODO: Is this the best representation, or does it make sense to just have a line
 -- defined by its endpoints? It feels like doing that may make some computations more
 -- complex than they need to be.
 data Line a = Line { point :: Point a, slope :: Point a }
 
+data Facet a = Facet { sides :: [Line a] }
+
 -- This should correspond to one line of G-code
 type Command = [String]
+
+-- Given a command, write it as one line of G-code
+showCommand :: Command -> String
+showCommand = map toUpper . unwords
 
 -- Add the coordinates of two points
 addPoints :: Num a => Point a -> Point a -> Point a
@@ -40,15 +46,15 @@ addPoints (Point x1 y1 z1) (Point x2 y2 z2) = Point (x1 + x2) (y1 + y2) (z1 + z2
 scalePoint :: Num a => a -> Point a -> Point a
 scalePoint = fmap . (*)
 
--- Given a command, write it as one line of G-code
-showCommand :: Command -> String
-showCommand = map toUpper . unwords
-
 -- Create a line given its endpoints
 lineFromEndpoints :: Num a => Point a -> Point a -> Line a
 lineFromEndpoints p1 p2 = Line p1 (addPoints (scalePoint (-1) p1) p2)
 
--- Find the point on a line for a given Z value. Note that this evaluates to 0
+-- Get the other endpoint
+endpoint :: Num a => Line a -> Point a
+endpoint l = addPoints (point l) (slope l)
+
+-- Find the point on a line for a given Z value. Note that this evaluates to Nothing
 -- in the case that there is no point with that Z value, or if that is the only
 -- Z value present in that line. The latter should be okay because the properties
 -- of our meshes mean that the two endpoints of our line should be captured by
@@ -89,15 +95,32 @@ makeLines l
     where l' = tail l
 
 -- Read a list of three coordinates (as strings separated by spaces) into the correct
--- lines
-readFacet :: (Num a, Read a) => [String] -> [Line a]
+-- Lines
+readFacet :: (Num a, Read a) => [String] -> Facet a
 readFacet f
     | length f < 3 = error "Invalid facet"
-    | otherwise = makeLines $ map readPoint f'
+    | otherwise = Facet $ makeLines $ map readPoint f'
     where f' = last f : f -- So that we're cyclic
 
 -- From STL file (as a list of Strings, each String corresponding to one line),
 -- produce a list of lists of Lines, where each list of Lines corresponds to a
 -- facet in the original STL
-facetLinesFromSTL :: (Num a, Read a) => [String] -> [[Line a]]
+facetLinesFromSTL :: (Num a, Read a) => [String] -> [Facet a]
 facetLinesFromSTL = map readFacet . map cleanupFacet . facetsFromSTL
+
+-- Determine if a triangle intersects a plane at a given z value
+triangleIntersects :: (Eq a, RealFrac a) => a -> Facet a -> [Point a]
+triangleIntersects v f = trimIntersections $ map fromJust $ filter (/= Nothing) intersections
+    where intersections = map (flip pointAtZValue v) (sides f)
+
+-- Get rid of the case where a triangle intersects the plane at one point
+trimIntersections :: Eq a => [Point a] -> [Point a]
+trimIntersections l
+    | length l' <= 1 = []
+    | otherwise = l'
+    where l' = nub l
+
+-- Find all the points in the mesh at a given z value
+-- Each list in the output should have length 2, corresponding to a line segment
+allIntersections :: (Eq a, RealFrac a) => a -> [Facet a] -> [[Point a]]
+allIntersections = map . triangleIntersects
