35eed20f509b1e6fed613887b71b7036f72c2752
Matthias Fischmann <mf@zerobuzz.net>
2021-06-12 08:36:21 +0200

    Trigger ghc warning (for testing).

--- Graphics/Slicer.hs
+++ Graphics/Slicer.hs
@@ -33,7 +33,7 @@ module Graphics.Slicer (
   module MaS
   ) where
 
-import Prelude ()
+import Prelude (head)
 
 import Graphics.Slicer.Math.Definitions as MD (Point2(Point2),Point3(Point3), Contour, distance, addPoints, scalePoint, roundPoint2)
 
