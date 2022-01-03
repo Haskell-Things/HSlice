4e3aeaeceee4b04ddd610e9bd2355051ff305c99
Julia Longtin <julia.longtin@gmail.com>
2022-01-03 21:30:47 +0000

    add initial golden tests.

---
+++ tests/golden/C2-Straight_Skeleton.ganja.js
@@ -0,0 +1,66 @@
+Algebra(2,0,1,()=>{
+  var line = (a,b,c)=>a*1e1 + b*1e2 + c*1e0;
+  var point = (x,y)=>!(1e0 + x*1e1 + y*1e2);
+  var aaa = point(-1.0,-1.0);
+  var aab = point(1.0,-1.0);
+  var aba = point(1.0,-1.0);
+  var abb = point(0.0,0.0);
+  var aca = point(0.0,0.0);
+  var acb = point(1.0,1.0);
+  var ada = point(1.0,1.0);
+  var adb = point(-1.0,1.0);
+  var aea = point(-1.0,1.0);
+  var aeb = point(-1.0,-1.0);
+  var afa = 0.7071067811865475e1-0.7071067811865475e2+0.0e0;
+  var afb = 0.3826834323650897e1+0.9238795325112867e2+0.541196100146197e0;
+  var afc = 0.9807852804032305e1+0.19509032201612836e2+0.4870636221857319e0;
+  var aga = -0.3826834323650897e1+0.9238795325112867e2-0.541196100146197e0;
+  var agb = -0.7071067811865475e1-0.7071067811865475e2+0.0e0;
+  var agc = -0.9807852804032305e1+0.19509032201612836e2-0.4870636221857319e0;
+  var aha = 0.9807852804032305e1+0.19509032201612836e2+0.4870636221857319e0;
+  var ahb = 0.0e1+1.414213562373095e2+0.0e0;
+  var ahc = -0.9807852804032305e1+0.19509032201612836e2-0.4870636221857319e0;
+  document.body.appendChild(this.graph([
+    0x882288,
+    [aaa,aab],
+    0x00AA88,
+    aaa, "aaa",
+    aab, "aab",
+    0x882288,
+    [aba,abb],
+    0x00AA88,
+    aba, "aba",
+    abb, "abb",
+    0x882288,
+    [aca,acb],
+    0x00AA88,
+    aca, "aca",
+    acb, "acb",
+    0x882288,
+    [ada,adb],
+    0x00AA88,
+    ada, "ada",
+    adb, "adb",
+    0x882288,
+    [aea,aeb],
+    0x00AA88,
+    aea, "aea",
+    aeb, "aeb",
+    afa, "afa",
+    afb, "afb",
+    afc, "afc",
+    aga, "aga",
+    agb, "agb",
+    agc, "agc",
+    aha, "aha",
+    ahb, "ahb",
+    ahc, "ahc",
+  ],{
+    grid: true,
+    labels: true,
+    lineWidth: 3,
+    pointRadius: 1,
+    fontSize: 1,
+    scale: 1,
+}));
+});
---
+++ tests/golden/C6-Straight_Skeleton.ganja.js
@@ -0,0 +1,92 @@
+Algebra(2,0,1,()=>{
+  var line = (a,b,c)=>a*1e1 + b*1e2 + c*1e0;
+  var point = (x,y)=>!(1e0 + x*1e1 + y*1e2);
+  var aaa = point(-1.0,1.0);
+  var aab = point(-1.0,-1.0);
+  var aba = point(-1.0,-1.0);
+  var abb = point(-0.5,-1.0);
+  var aca = point(-0.5,-1.0);
+  var acb = point(0.0,0.0);
+  var ada = point(0.0,0.0);
+  var adb = point(0.5,-1.0);
+  var aea = point(0.5,-1.0);
+  var aeb = point(1.0,-1.0);
+  var afa = point(1.0,-1.0);
+  var afb = point(1.0,1.0);
+  var aga = point(1.0,1.0);
+  var agb = point(-1.0,1.0);
+  var aha = 0.7071067811865475e1-0.7071067811865475e2+0.0e0;
+  var ahb = 0.8506508083520399e1+0.5257311121191337e2+0.9510565162951536e0;
+  var ahc = 0.9932897335288758e1-0.11565251949756605e2+0.606432399999752e0;
+  var aia = 0.8506508083520399e1-0.5257311121191337e2-0.9510565162951536e0;
+  var aib = 0.7071067811865475e1+0.7071067811865475e2+0.0e0;
+  var aic = 0.9932897335288758e1+0.11565251949756605e2-0.606432399999752e0;
+  var aja = -0.7071067811865475e1-0.7071067811865475e2+0.0e0;
+  var ajb = 0.9932897335288758e1-0.11565251949756605e2+0.606432399999752e0;
+  var ajc = 0.328526568895664e1-0.9444947292227959e2+0.6961601101968017e0;
+  var aka = 0.9932897335288758e1+0.11565251949756605e2-0.606432399999752e0;
+  var akb = -0.7071067811865475e1+0.7071067811865475e2+0.0e0;
+  var akc = 0.328526568895664e1+0.9444947292227959e2-0.6961601101968017e0;
+  var ala = 0.328526568895664e1-0.9444947292227959e2+0.6961601101968017e0;
+  var alb = 1.7888543819998317e1+0.0e2+0.0e0;
+  var alc = 0.328526568895664e1+0.9444947292227959e2-0.6961601101968017e0;
+  document.body.appendChild(this.graph([
+    0x882288,
+    [aaa,aab],
+    0x00AA88,
+    aaa, "aaa",
+    aab, "aab",
+    0x882288,
+    [aba,abb],
+    0x00AA88,
+    aba, "aba",
+    abb, "abb",
+    0x882288,
+    [aca,acb],
+    0x00AA88,
+    aca, "aca",
+    acb, "acb",
+    0x882288,
+    [ada,adb],
+    0x00AA88,
+    ada, "ada",
+    adb, "adb",
+    0x882288,
+    [aea,aeb],
+    0x00AA88,
+    aea, "aea",
+    aeb, "aeb",
+    0x882288,
+    [afa,afb],
+    0x00AA88,
+    afa, "afa",
+    afb, "afb",
+    0x882288,
+    [aga,agb],
+    0x00AA88,
+    aga, "aga",
+    agb, "agb",
+    aha, "aha",
+    ahb, "ahb",
+    ahc, "ahc",
+    aia, "aia",
+    aib, "aib",
+    aic, "aic",
+    aja, "aja",
+    ajb, "ajb",
+    ajc, "ajc",
+    aka, "aka",
+    akb, "akb",
+    akc, "akc",
+    ala, "ala",
+    alb, "alb",
+    alc, "alc",
+  ],{
+    grid: true,
+    labels: true,
+    lineWidth: 3,
+    pointRadius: 1,
+    fontSize: 1,
+    scale: 1,
+}));
+});
