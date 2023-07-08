02b44bfb357c5e5ac958f20d0463124f4f44f288
Julia Longtin <julia.longtin@gmail.com>
2023-07-08 22:21:56 +0000

    fix concave chevron quad tests, so that generated objects are always bisectable.

--- tests/Math/Geometry/ConcaveChevronQuad.hs
+++ tests/Math/Geometry/ConcaveChevronQuad.hs
@@ -23,10 +23,10 @@ module Math.Geometry.ConcaveChevronQuad (
   concaveChevronQuadSpec
   ) where
 
-import Prelude (Bool(True), Show(show), ($), (<>), (==), error, length, otherwise)
+import Prelude (Bool(True), Show(show), ($), (<>), (==), error, head, length, otherwise)
 
 -- The Maybe library.
-import Data.Maybe (fromJust, fromMaybe)
+import Data.Maybe (fromJust, fromMaybe, isJust)
 
 -- Slists, a form of list with a stated size in the structure.
 import Slist (slist, len)
@@ -44,22 +44,31 @@ import Graphics.Slicer (ℝ)
 import Graphics.Slicer.Math.Definitions (Contour)
 
 -- Our serialization library, for debugging,
-import Graphics.Slicer.Math.Ganja (dumpGanja)
+import Graphics.Slicer.Math.Ganja (dumpGanja, dumpGanjas, toGanja)
+
+import Graphics.Slicer.Math.PGA (outAndErrOf, plinesIntersectIn)
 
 -- The functions for generating random geometry, for testing purposes.
-import Graphics.Slicer.Math.RandomGeometry (Radian(Radian), cellFrom, generationsOf, justSupported, oneNodeTreeOf, onlyOne, randomConcaveChevronQuad, remainderFrom)
+import Graphics.Slicer.Math.RandomGeometry (Radian(Radian), cellFrom, generationsOf, oneNodeTreeOf, onlyOne, randomConcaveChevronQuad, remainderFrom)
 
 -- Our logic for dividing a contour into cells, which each get nodetrees for them, which are combined into a straight skeleton.
-import Graphics.Slicer.Math.Skeleton.Cells (findDivisions, findFirstCellOfContour, findNextCell, getNodeTreeOfCell)
+import Graphics.Slicer.Math.Skeleton.Cells (findDivisions, findFirstCellOfContour, findNextCell, getRawNodeTreeOfCell, landingPointOf)
+
+import Graphics.Slicer.Math.Skeleton.Concave (eNodesOfOutsideContour)
+
+import Graphics.Slicer.Math.Skeleton.Definitions (MotorcycleIntersection(WithENode))
+
+-- The part of our library that puts faces onto a contour. faces have one exterior side, and a number of internal sides (defined by Arcs).
+import Graphics.Slicer.Math.Skeleton.Face (facesOf)
 
 -- The portion of our library that reasons about motorcycles, emiting from the concave nodes of our contour.
-import Graphics.Slicer.Math.Skeleton.Motorcycles (convexMotorcycles, crashMotorcycles)
+import Graphics.Slicer.Math.Skeleton.Motorcycles (convexMotorcycles, crashMotorcycles, motorcycleIntersectsAt)
 
 -- The entry point for getting the straight skeleton of a contour.
 import Graphics.Slicer.Math.Skeleton.Skeleton (findStraightSkeleton)
 
 -- Shared tests, between different geometry.
-import Math.Geometry.CommonTests (prop_CanPlaceFaces, prop_FacesInOrder, prop_HasFourFaces, prop_HasAStraightSkeleton, prop_StraightSkeletonHasOneNodeTree)
+import Math.Geometry.CommonTests (prop_CanPlaceFaces, prop_FacesInOrder, prop_HasFourFaces, prop_HasAStraightSkeleton, prop_StraightSkeletonHasOneNodeTree, prop_NodeTreeHasFewerThanThreeGenerations)
 
 -- Our Utility library, for making these tests easier to read.
 import Math.Util ((-->))
@@ -70,102 +79,174 @@ prop_OneMotorcycle contour = length (convexMotorcycles contour) == 1
 prop_OneDivide :: Contour -> Expectation
 prop_OneDivide contour = len (slist $ findDivisions contour (fromMaybe (error $ show contour) $ crashMotorcycles contour [])) --> 1
 
-prop_NodeTreeHasTwoGenerations :: Contour -> Expectation
-prop_NodeTreeHasTwoGenerations contour = generationsOf (oneNodeTreeOf $ fromMaybe (error "no straight skeleton?") $ findStraightSkeleton contour []) --> 2
-
--- | insane result of filterAllIntersections
 {-
-also happens with:
-         0.0
-         0.0
-         Radian {getRadian = 1.0}
-         Radian {getRadian = 0.1}
-         Positive {getPositive = 1.0e-2}
-         Positive {getPositive = 29.0}
+prop_ConcaveChevronQuadSample :: Contour -> Expectation
+prop_ConcaveChevronQuadSample contour = error $ "dumping: \n"
+                                             <> dumpGanjas [toGanja contour, toGanja (head $ convexMotorcycles contour)] <> "\n"
 -}
-{-
-also happens with:
-         0.0
-         0.0
-         Radian {getRadian = 1.0}
-         Radian {getRadian = 1.0}
-         Positive {getPositive = 16.0}
-         Positive {getPositive = 3.0e-3}
--}
-unit_ConcaveChevronQuadNodeTreeHasTwoGenerations :: Bool
-unit_ConcaveChevronQuadNodeTreeHasTwoGenerations
-   | res == 2 = True
-   | otherwise = error $ "returned " <> show res <> ".\n"
-                      <> show nodeTreeOfFirstCell <> "\n"
-                      <> dumpGanja nodeTreeOfFirstCell <> "\n"
-                      <> dumpGanja nodeTreeOfSecondCell <> "\n"
-  where
-    res = generationsOf $ oneNodeTreeOf $ fromMaybe (error "no straight skeleton?") $ findStraightSkeleton contour []
-    nodeTreeOfFirstCell = justSupported $ getNodeTreeOfCell $ cellFrom $ findFirstCellOfContour contour $ findDivisions contour $ fromMaybe (error "failed to crash") $ crashMotorcycles contour []
-    nodeTreeOfSecondCell = justSupported $ getNodeTreeOfCell $ cellFrom $ findNextCell $ onlyOne $ fromJust $ remainderFrom $ findFirstCellOfContour contour $ findDivisions contour $ fromMaybe (error "failed to crash") $ crashMotorcycles contour []
-    contour = randomConcaveChevronQuad x y tilt1 tilt2 distance1 distance2
-    x,y :: ℝ
-    x = 0
-    y = 0
-    tilt1 = Radian 2.0
-    tilt2 = Radian 1.0
-    distance1,distance2 :: Positive ℝ
-    distance1 = 1.0e-4
-    distance2 = 1.0
 
--- was falsifiable. should return 2.
-unit_ConcaveChevronQuadNodeTreeHasTwoGenerations_2 :: Bool
-unit_ConcaveChevronQuadNodeTreeHasTwoGenerations_2
-  | res == 2 = True
-  | otherwise = error $ "returned " <> show res <> ".\n"
-                      <> show firstCell <> "\n"
-                      <> dumpGanja firstCell <> "\n"
-                      <> dumpGanja nodeTreeOfSecondCell <> "\n"
+unit_ConcaveChevronQuadHasAStraightSkeleton :: Bool
+unit_ConcaveChevronQuadHasAStraightSkeleton
+  | isJust (findStraightSkeleton contour []) = True
+  | otherwise = error $ "dumping: \n"
+                <> dumpGanjas [toGanja contour, toGanja $ head $ convexMotorcycles contour] <> "\n"
+                <> show (landingPointOf contour motorcycle) <> "\n"
+                <> show (plinesIntersectIn (outAndErrOf motorcycle) (outAndErrOf eNode)) <> "\n"
+                <> show divides <> "\n"
   where
-    res = generationsOf nodeTree
-    nodeTree = oneNodeTreeOf $ fromMaybe (error "no straight skeleton?") $ findStraightSkeleton contour []
-    firstCell = cellFrom $ findFirstCellOfContour contour $ findDivisions contour $ fromMaybe (error "failed to crash") $ crashMotorcycles contour []
-    nodeTreeOfSecondCell = justSupported $ getNodeTreeOfCell $ cellFrom $ findNextCell $ onlyOne $ fromJust $ remainderFrom $ findFirstCellOfContour contour $ findDivisions contour $ fromMaybe (error "failed to crash") $ crashMotorcycles contour []
-    contour = randomConcaveChevronQuad x y tilt1 tilt2 distance1 distance2
-    x,y :: ℝ
-    x = 0
-    y = 0
-    tilt1 = Radian 2.0
-    tilt2 = Radian 2.0
-    distance1,distance2 :: Positive ℝ
-    distance1 = 1.0
-    distance2 = 1.0
-
--- was falsifiable. should return 2.
-unit_ConcaveChevronQuadNodeTreeHasTwoGenerations_3 :: Bool
-unit_ConcaveChevronQuadNodeTreeHasTwoGenerations_3
-  | res == 2 = True
-  | otherwise = error $ "returned " <> show res <> ".\n"
-                      <> show firstCell <> "\n"
-                      <> dumpGanja firstCell <> "\n"
-                      <> dumpGanja nodeTreeOfFirstCell <> "\n"
-                      <> dumpGanja nodeTreeOfSecondCell <> "\n"
-  where
-    res = generationsOf nodeTree
-    nodeTree = oneNodeTreeOf $ fromMaybe (error "no straight skeleton?") $ findStraightSkeleton contour []
-    firstCell = cellFrom $ findFirstCellOfContour contour $ findDivisions contour $ fromMaybe (error "failed to crash") $ crashMotorcycles contour []
-    nodeTreeOfFirstCell = justSupported $ getNodeTreeOfCell $ cellFrom $ findFirstCellOfContour contour $ findDivisions contour $ fromMaybe (error "failed to crash") $ crashMotorcycles contour []
-    nodeTreeOfSecondCell = justSupported $ getNodeTreeOfCell $ cellFrom $ findNextCell $ onlyOne $ fromJust $ remainderFrom $ findFirstCellOfContour contour $ findDivisions contour $ fromMaybe (error "failed to crash") $ crashMotorcycles contour []
-    contour = randomConcaveChevronQuad x y tilt1 tilt2 distance1 distance2
+    eNode = (\(WithENode a) -> a) $ landingPointOf contour motorcycle
+    divides = findDivisions contour (fromMaybe (error "no") $ crashMotorcycles contour []) 
+    motorcycle = head $ convexMotorcycles contour
+    contour = randomConcaveChevronQuad x y tilt1 distance1 distance2
     x,y :: ℝ
     x = 0
     y = 0
     tilt1 = Radian 1.0
-    tilt2 = Radian 0.1
     distance1,distance2 :: Positive ℝ
-    distance1 = 0.1
-    distance2 = 0.1
+    distance1 = 1.0
+    distance2 = 1.0
+
+unit_ConcaveChevronQuadNodeTreeHasOneGeneration :: Bool
+unit_ConcaveChevronQuadNodeTreeHasOneGeneration
+   | res == 1 = True
+   | otherwise = error $ "returned " <> show res <> ".\n"
+                      <> show nodeTreeOfFirstCell <> "\n"
+                      <> show nodeTreeOfSecondCell <> "\n"
+                      <> show finalNodeTree <> "\n"
+                      <> dumpGanja nodeTreeOfFirstCell <> "\n"
+                      <> dumpGanja nodeTreeOfSecondCell <> "\n"
+                      <> dumpGanja (fromJust $ findStraightSkeleton contour []) <> "\n"
+  where
+    res = generationsOf finalNodeTree
+    finalNodeTree = oneNodeTreeOf $ fromMaybe (error "no straight skeleton?") $ findStraightSkeleton contour []
+    nodeTreeOfFirstCell = getRawNodeTreeOfCell $ cellFrom $ findFirstCellOfContour contour $ findDivisions contour $ fromMaybe (error "failed to crash") $ crashMotorcycles contour []
+    nodeTreeOfSecondCell = getRawNodeTreeOfCell $ cellFrom $ findNextCell $ onlyOne $ fromJust $ remainderFrom $ findFirstCellOfContour contour $ findDivisions contour $ fromMaybe (error "failed to crash") $ crashMotorcycles contour []
+    contour = randomConcaveChevronQuad x y tilt1 distance1 distance2
+    x,y :: ℝ
+    x = 0
+    y = 0
+    tilt1 = Radian 1.0
+    distance1,distance2 :: Positive ℝ
+    distance1 = 1.0
+    distance2 = 1.0
+
+-- Failed, until we changed filterAllIntersections to always look at the middle object.
+unit_ConcaveChevronQuadNodeTreeHasOneGeneration_2 :: Bool
+unit_ConcaveChevronQuadNodeTreeHasOneGeneration_2
+  | res == 1 = True
+  | otherwise = error $ "returned:\n"
+                     <> show res <> ".\n"
+                      <> show firstCell <> "\n"
+                      <> show divisions <> "\n"
+                      <> dumpGanjas [toGanja motorcycle, toGanja contour] <> "\n"
+  where
+    res = generationsOf finalNodeTree
+    finalNodeTree = oneNodeTreeOf $ fromMaybe (error "no straight skeleton?") $ findStraightSkeleton contour []
+    firstCell = findFirstCellOfContour contour divisions 
+    divisions = findDivisions contour $ fromMaybe (error "failed to crash") $ crashMotorcycles contour []
+    motorcycle = head $ convexMotorcycles contour
+    contour = randomConcaveChevronQuad x y tilt1 distance1 distance2
+    x,y :: ℝ
+    x = 0
+    y = 0
+    tilt1 = Radian 0.1
+    distance1,distance2 :: Positive ℝ
+    distance1 = 6.0
+    distance2 = 1.0e-3
+
+
+unit_ConcaveChevronQuadSample :: Bool
+unit_ConcaveChevronQuadSample = error $ "dumping: \n"
+                                             <> dumpGanjas [toGanja contour, toGanja (head $ convexMotorcycles contour)] <> "\n"
+  where
+    contour = randomConcaveChevronQuad x y tilt1 distance1 distance2
+    x,y :: ℝ
+    x = 0
+    y = 0
+    tilt1 = Radian 0.1
+    distance1,distance2 :: Positive ℝ
+    distance1 = 2.0
+    distance2 = 1.0e-4
+
+{-
+       uncaught exception: ErrorCall
+-}
+unit_ConcaveChevronQuadNodeTreeHasOneGeneration_3 :: Bool
+unit_ConcaveChevronQuadNodeTreeHasOneGeneration_3
+  | res == 1 = True
+  | otherwise = error $ "returned:\n"
+                      <> show firstCell <> "\n"
+                      <> dumpGanjas [toGanja finalNodeTree] <> "\n"
+  where
+    res = generationsOf finalNodeTree
+    finalNodeTree = oneNodeTreeOf $ fromMaybe (error "no straight skeleton?") $ findStraightSkeleton contour []
+    firstCell = findFirstCellOfContour contour divisions 
+    divisions = findDivisions contour $ fromMaybe (error "failed to crash") $ crashMotorcycles contour []
+    contour = randomConcaveChevronQuad x y tilt1 distance1 distance2
+    x,y :: ℝ
+    x = 0
+    y = 0
+    tilt1 = Radian 0.1
+    distance1,distance2 :: Positive ℝ
+    distance1 = 2.0
+    distance2 = 1.0e-4
+
+-- | was broken, until we stopped treating a motorcycle<->ENode intersection like a side.
+unit_ConcaveChevronQuadCanPlaceFaces :: Bool
+unit_ConcaveChevronQuadCanPlaceFaces
+  | length faces == 4 = True
+  | otherwise = error $ "dumping: \n"
+                <> dumpGanjas [toGanja contour, toGanja $ head $ convexMotorcycles contour] <> "\n"
+                <> show (landingPointOf contour motorcycle) <> "\n"
+                <> show (plinesIntersectIn (outAndErrOf motorcycle) (outAndErrOf eNode)) <> "\n"
+                <> show divides <> "\n"
+  where
+    faces = facesOf skeleton
+    skeleton = fromJust $ findStraightSkeleton contour []
+    eNode = (\(WithENode a) -> a) $ landingPointOf contour motorcycle
+    divides = findDivisions contour (fromMaybe (error "no") $ crashMotorcycles contour []) 
+    motorcycle = head $ convexMotorcycles contour
+    contour = randomConcaveChevronQuad x y tilt1 distance1 distance2
+    x,y :: ℝ
+    x = 0
+    y = 0
+    tilt1 = Radian 1.0
+    distance1,distance2 :: Positive ℝ
+    distance1 = 1.0
+    distance2 = 1.0
+
+-- | was broken, until we did a logical check for if a motorcycle intersects a line segment that is part of an anticollinear enode.
+unit_ConcaveChevronQuadCanPlaceFaces_2 :: Bool
+unit_ConcaveChevronQuadCanPlaceFaces_2
+  | length faces == 4 = True
+  | otherwise = error $ "dumping: \n"
+                <> show motorcycle <> "\n"
+                <> show targetENode <> "\n"
+                <> show (plinesIntersectIn (outAndErrOf motorcycle) (outAndErrOf targetENode)) <> "\n"
+                <> dumpGanjas [toGanja motorcycle, toGanja targetENode] <> "\n"
+                <> show (landingPointOf contour motorcycle) <> "\n"
+                <> show (motorcycleIntersectsAt contour motorcycle) <> "\n"
+                <> show divides <> "\n"
+  where
+    faces = facesOf skeleton
+    skeleton = fromJust $ findStraightSkeleton contour []
+    divides = findDivisions contour (fromMaybe (error "no") $ crashMotorcycles contour [])
+    [_, targetENode, _] = eNodes
+    eNodes = eNodesOfOutsideContour contour
+    motorcycle = head $ convexMotorcycles contour
+    contour = randomConcaveChevronQuad x y tilt1 distance1 distance2
+    x,y :: ℝ
+    x = 0
+    y = 0
+    tilt1 = Radian 0.2
+    distance1,distance2 :: Positive ℝ
+    distance1 = 3.0
+    distance2 = 0.5
 
 concaveChevronQuadBrokenSpec :: Spec
-concaveChevronQuadBrokenSpec = do
-  describe "Geometry (Concave Chevron Quads)" $ do
-    it "generates two generations of INodes(unit)" $
-      unit_ConcaveChevronQuadNodeTreeHasTwoGenerations
+concaveChevronQuadBrokenSpec =
+    it "always fails, dumping a sample" $
+      unit_ConcaveChevronQuadSample
 
 concaveChevronQuadSpec :: Spec
 concaveChevronQuadSpec = do
@@ -174,16 +255,24 @@ concaveChevronQuadSpec = do
       property (boolFromConcaveChevronQuad prop_OneMotorcycle)
     it "finds one divide" $
       property (expectationFromConcaveChevronQuad prop_OneDivide)
+    it "finds a straight skeleton (unit)" $
+      unit_ConcaveChevronQuadHasAStraightSkeleton
     it "finds a straight skeleton" $
       property (expectationFromConcaveChevronQuad prop_HasAStraightSkeleton)
     it "only finds one nodetree in the straight skeleton" $
       property (expectationFromConcaveChevronQuad prop_StraightSkeletonHasOneNodeTree)
-    it "generates two generations of INodes" $
-      property (expectationFromConcaveChevronQuad prop_NodeTreeHasTwoGenerations)
-    it "generates two generations of INodes(unit)" $
-      unit_ConcaveChevronQuadNodeTreeHasTwoGenerations_2
-    it "generates two generations of INodes(unit 3)" $
-      unit_ConcaveChevronQuadNodeTreeHasTwoGenerations_3
+    it "generates one generation of INodes (unit)" $
+      unit_ConcaveChevronQuadNodeTreeHasOneGeneration
+    it "generates one generation of INodes (unit 2)" $
+      unit_ConcaveChevronQuadNodeTreeHasOneGeneration_2
+    it "generates one generation of INodes (unit 3)" $
+      unit_ConcaveChevronQuadNodeTreeHasOneGeneration_3
+    it "generates one or two generations of INodes" $
+      property (boolFromConcaveChevronQuad prop_NodeTreeHasFewerThanThreeGenerations)
+    it "can place faces on the straight skeleton (unit)" $
+      unit_ConcaveChevronQuadCanPlaceFaces
+    it "can place faces on the straight skeleton (unit 2)" $
+      unit_ConcaveChevronQuadCanPlaceFaces_2
     it "can place faces on the straight skeleton" $
       property (expectationFromConcaveChevronQuad prop_CanPlaceFaces)
     it "only places four faces" $
@@ -192,11 +281,11 @@ concaveChevronQuadSpec = do
       property (expectationFromConcaveChevronQuad prop_FacesInOrder)
     -- NOTE: faces on a concave figgure can end up not all wound left.
   where
-    boolFromConcaveChevronQuad :: (Contour -> Bool) -> ℝ -> ℝ -> Radian ℝ -> Radian ℝ -> Positive ℝ -> Positive ℝ -> Bool
-    boolFromConcaveChevronQuad f x y rawFirstTilt rawSecondTilt rawFirstDistanceToCorner rawSecondDistanceToCorner = f concaveChevronQuad
+    boolFromConcaveChevronQuad :: (Contour -> Bool) -> ℝ -> ℝ -> Radian ℝ -> Positive ℝ -> Positive ℝ -> Bool
+    boolFromConcaveChevronQuad f x y rawFirstTilt rawFirstDistanceToCorner rawSecondDistanceToCorner = f concaveChevronQuad
       where
-        concaveChevronQuad = randomConcaveChevronQuad x y rawFirstTilt rawSecondTilt rawFirstDistanceToCorner rawSecondDistanceToCorner
-    expectationFromConcaveChevronQuad :: (Contour -> Expectation) -> ℝ -> ℝ -> Radian ℝ -> Radian ℝ -> Positive ℝ -> Positive ℝ -> Expectation
-    expectationFromConcaveChevronQuad f x y rawFirstTilt rawSecondTilt rawFirstDistanceToCorner rawSecondDistanceToCorner = f concaveChevronQuad
+        concaveChevronQuad = randomConcaveChevronQuad x y rawFirstTilt rawFirstDistanceToCorner rawSecondDistanceToCorner
+    expectationFromConcaveChevronQuad :: (Contour -> Expectation) -> ℝ -> ℝ -> Radian ℝ -> Positive ℝ -> Positive ℝ -> Expectation
+    expectationFromConcaveChevronQuad f x y rawFirstTilt rawFirstDistanceToCorner rawSecondDistanceToCorner = f concaveChevronQuad
       where
-        concaveChevronQuad = randomConcaveChevronQuad x y rawFirstTilt rawSecondTilt rawFirstDistanceToCorner rawSecondDistanceToCorner
+        concaveChevronQuad = randomConcaveChevronQuad x y rawFirstTilt rawFirstDistanceToCorner rawSecondDistanceToCorner
