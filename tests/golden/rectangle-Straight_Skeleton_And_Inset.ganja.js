e1783da5f3df1de3b0944bd82d727035799078f8
Julia Longtin <julia.longtin@gmail.com>
2023-08-22 21:18:27 +0000

    add some new golden tests.

---
+++ tests/golden/rectangle-Straight_Skeleton_And_Inset.ganja.js
@@ -0,0 +1,66 @@
+Algebra(2,0,1,()=>{
+  var line = (a,b,c)=>a*1e1 + b*1e2 + c*1e0;
+  var point = (x,y)=>!(1e0 + x*1e1 + y*1e2);
+  var aaa = point(-1.5,-1.0);
+  var aab = point(1.5,-1.0);
+  var aba = point(1.5,-1.0);
+  var abb = point(1.5,1.0);
+  var aca = point(1.5,1.0);
+  var acb = point(-1.5,1.0);
+  var ada = point(-1.5,1.0);
+  var adb = point(-1.5,-1.0);
+  var aea = 0.7071067811865475e1+0.7071067811865475e2-0.35355339059327373e0;
+  var aeb = -0.7071067811865475e1+0.7071067811865475e2+0.35355339059327373e0;
+  var aec = 0.0e1+1.0e2+0.0e0;
+  var afa = 0.7071067811865475e1-0.7071067811865475e2+0.35355339059327373e0;
+  var afb = 0.0e1+1.0e2+0.0e0;
+  var afc = -0.7071067811865475e1-0.7071067811865475e2-0.35355339059327373e0;
+  var ba = point(1.4,0.9000000000000001);
+  var bb = point(-1.4,0.9000000000000001);
+  var bc = point(-1.4,-0.9000000000000001);
+  var bd = point(1.4,-0.9000000000000001);
+  document.body.appendChild(this.graph([
+    0x882288,
+    [aaa,aab],
+    0x00AA88,
+    aaa, "aaa",
+    aab, "aab",
+    0x882288,
+    [aba,abb],
+    0x00AA88,
+    aba, "aba",
+    abb, "abb",
+    0x882288,
+    [aca,acb],
+    0x00AA88,
+    aca, "aca",
+    acb, "acb",
+    0x882288,
+    [ada,adb],
+    0x00AA88,
+    ada, "ada",
+    adb, "adb",
+    aea, "aea",
+    aeb, "aeb",
+    aec, "aec",
+    afa, "afa",
+    afb, "afb",
+    afc, "afc",
+    ba, "ba",
+    bb, "bb",
+    bc, "bc",
+    bd, "bd",
+    0x882288,
+    [ba,bb],
+    [bb,bc],
+    [bc,bd],
+    [bd,ba],
+  ],{
+    grid: true,
+    labels: true,
+    lineWidth: 3,
+    pointRadius: 1,
+    fontSize: 1,
+    scale: 1,
+}));
+});
---
+++ tests/golden/triangle-Straight_Skeleton_And_Insets.ganja.js
@@ -0,0 +1,145 @@
+Algebra(2,0,1,()=>{
+  var line = (a,b,c)=>a*1e1 + b*1e2 + c*1e0;
+  var point = (x,y)=>!(1e0 + x*1e1 + y*1e2);
+  var aaa = point(1.0,1.7320508075688772);
+  var aab = point(0.0,0.0);
+  var aba = point(0.0,0.0);
+  var abb = point(2.0,0.0);
+  var aca = point(2.0,0.0);
+  var acb = point(1.0,1.7320508075688772);
+  var ada = -1.0e1+0.0e2+1.0e0;
+  var adb = 0.5000000000000001e1-0.8660254037844387e2+0.0e0;
+  var adc = 0.5000000000000001e1+0.8660254037844387e2-1.0000000000000002e0;
+  var ba = point(1.8267949192431119,0.1);
+  var bb = point(1.0,1.532050807568877);
+  var ca = point(1.0,1.532050807568877);
+  var cb = point(0.17320508075688776,0.1);
+  var da = point(0.17320508075688776,0.1);
+  var db = point(1.8267949192431119,0.1);
+  var ea = point(1.6535898384862242,0.20000000000000004);
+  var eb = point(0.9999999999999998,1.3320508075688768);
+  var fa = point(0.9999999999999998,1.3320508075688768);
+  var fb = point(0.34641016151377546,0.20000000000000004);
+  var ga = point(0.34641016151377546,0.20000000000000004);
+  var gb = point(1.6535898384862242,0.20000000000000004);
+  var ha = point(1.4803847577293368,0.3000000000000001);
+  var hb = point(1.0,1.132050807568877);
+  var ia = point(1.0,1.132050807568877);
+  var ib = point(0.5196152422706632,0.3000000000000001);
+  var ja = point(0.5196152422706632,0.3000000000000001);
+  var jb = point(1.4803847577293368,0.3000000000000001);
+  var ka = point(1.3071796769724493,0.4000000000000001);
+  var kb = point(1.0,0.9320508075688774);
+  var la = point(1.0,0.9320508075688774);
+  var lb = point(0.6928203230275508,0.4000000000000001);
+  var ma = point(0.6928203230275508,0.4000000000000001);
+  var mb = point(1.3071796769724493,0.4000000000000001);
+  var na = point(1.133974596215561,0.5);
+  var nb = point(1.0,0.7320508075688773);
+  var oa = point(1.0,0.7320508075688773);
+  var ob = point(0.8660254037844386,0.5);
+  var pa = point(0.8660254037844386,0.5);
+  var pb = point(1.133974596215561,0.5);
+  document.body.appendChild(this.graph([
+    0x882288,
+    [aaa,aab],
+    0x00AA88,
+    aaa, "aaa",
+    aab, "aab",
+    0x882288,
+    [aba,abb],
+    0x00AA88,
+    aba, "aba",
+    abb, "abb",
+    0x882288,
+    [aca,acb],
+    0x00AA88,
+    aca, "aca",
+    acb, "acb",
+    ada, "ada",
+    adb, "adb",
+    adc, "adc",
+    0x882288,
+    [ba,bb],
+    0x00AA88,
+    ba, "ba",
+    bb, "bb",
+    0x882288,
+    [ca,cb],
+    0x00AA88,
+    ca, "ca",
+    cb, "cb",
+    0x882288,
+    [da,db],
+    0x00AA88,
+    da, "da",
+    db, "db",
+    0x882288,
+    [ea,eb],
+    0x00AA88,
+    ea, "ea",
+    eb, "eb",
+    0x882288,
+    [fa,fb],
+    0x00AA88,
+    fa, "fa",
+    fb, "fb",
+    0x882288,
+    [ga,gb],
+    0x00AA88,
+    ga, "ga",
+    gb, "gb",
+    0x882288,
+    [ha,hb],
+    0x00AA88,
+    ha, "ha",
+    hb, "hb",
+    0x882288,
+    [ia,ib],
+    0x00AA88,
+    ia, "ia",
+    ib, "ib",
+    0x882288,
+    [ja,jb],
+    0x00AA88,
+    ja, "ja",
+    jb, "jb",
+    0x882288,
+    [ka,kb],
+    0x00AA88,
+    ka, "ka",
+    kb, "kb",
+    0x882288,
+    [la,lb],
+    0x00AA88,
+    la, "la",
+    lb, "lb",
+    0x882288,
+    [ma,mb],
+    0x00AA88,
+    ma, "ma",
+    mb, "mb",
+    0x882288,
+    [na,nb],
+    0x00AA88,
+    na, "na",
+    nb, "nb",
+    0x882288,
+    [oa,ob],
+    0x00AA88,
+    oa, "oa",
+    ob, "ob",
+    0x882288,
+    [pa,pb],
+    0x00AA88,
+    pa, "pa",
+    pb, "pb",
+  ],{
+    grid: true,
+    labels: true,
+    lineWidth: 3,
+    pointRadius: 1,
+    fontSize: 1,
+    scale: 1,
+}));
+});
